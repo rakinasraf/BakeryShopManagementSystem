a2435e087e8486c5b66a7df20746bdbf8008e1cd
rakinasraf <rakinasraf128@gmail.com>
2026-06-13 12:51:09 +0600

    Update project C#

---
+++ OOP2FinalProject/DataAccess.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2FinalProject
+{
+    public class DataAccess
+    {
+        private SqlConnection sqlcon;
+        private SqlCommand sqlcom;
+        private SqlDataAdapter sda;
+        private DataSet ds;
+
+        public SqlConnection Sqlcon
+        {
+            get { return this.sqlcon; }
+            set { this.sqlcon = value; }
+        }
+        public SqlCommand Sqlcom
+        {
+            get { return this.sqlcom; }
+            set { this.sqlcom = value; }
+        }
+        public SqlDataAdapter Sda
+        {
+            get { return this.sda; }
+            set { this.sda = value; }
+        }
+        public DataSet Ds
+        {
+            get { return this.ds; }
+            set { this.ds = value; }
+        }
+        public DataAccess()
+        {
+            this.Sqlcon = new SqlConnection("Data Source=LAPTOP-6I8C99IJ\\SQLEXPRESS;Initial Catalog=UserInfo;Integrated Security=True");
+            sqlcon.Open();
+        }
+
+        private void QueryText(string query)
+        {
+            this.Sqlcom = new SqlCommand(query, this.Sqlcon);
+        }
+
+        public DataSet ExecuteQuery(string sql)
+        {
+            this.Sqlcom = new SqlCommand(sql, this.Sqlcon);
+            this.Sda = new SqlDataAdapter(this.Sqlcom);
+            this.Ds = new DataSet();
+            this.Sda.Fill(this.Ds);
+            return Ds;
+        }
+        public DataTable ExecuteQueryTable(string sql)
+        {
+            this.Sqlcom = new SqlCommand(sql, this.Sqlcon);
+            this.Sda = new SqlDataAdapter(this.Sqlcom);
+            this.Ds = new DataSet();
+            this.Sda.Fill(this.Ds);
+            return Ds.Tables[0];
+        }
+
+        public int ExecuteDMLQuery(string sql)
+        {
+            this.Sqlcom = new SqlCommand(sql, this.Sqlcon);
+            int u = this.Sqlcom.ExecuteNonQuery();
+            return u;
+        }
+
+        public void ExecuteDDLQuery(string sql)
+        {
+            this.Sqlcom = new SqlCommand(sql, this.Sqlcon);
+            int u = this.Sqlcom.ExecuteNonQuery();
+            //return u;
+        }
+    }
+}
+//this.QueryText(sql);
