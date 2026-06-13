2d026a00dbf9a80464e89f555a32813f8a45707d
rakinasraf <rakinasraf128@gmail.com>
2026-06-13 12:53:29 +0600

    Update Staff Profile Form

---
+++ OOP2FinalProject/EditStaffProfile.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace OOP2FinalProject
+{
+    partial class EditStaffProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlStaffEditInfo = new System.Windows.Forms.Panel();
+            this.pnlStaffInfo = new System.Windows.Forms.Panel();
+            this.lblStaffId = new System.Windows.Forms.Label();
+            this.btnEditStaffSave = new System.Windows.Forms.Button();
+            this.lblEditStaffinfo = new System.Windows.Forms.Label();
+            this.txtEditStaffPassword = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.txtEditStaffEmail = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.txtEditStaffAddress = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.txtEditStaffMobileno = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.txtEditStaffName = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtEditStaffgender = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.pnlStaffEditInfo.SuspendLayout();
+            this.pnlStaffInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlStaffEditInfo
+            // 
+            this.pnlStaffEditInfo.BackColor = System.Drawing.Color.Peru;
+            this.pnlStaffEditInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlStaffEditInfo.Controls.Add(this.pnlStaffInfo);
+            this.pnlStaffEditInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlStaffEditInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlStaffEditInfo.Location = new System.Drawing.Point(0, 0);
+            this.pnlStaffEditInfo.Name = "pnlStaffEditInfo";
+            this.pnlStaffEditInfo.Size = new System.Drawing.Size(734, 461);
+            this.pnlStaffEditInfo.TabIndex = 231;
+            // 
+            // pnlStaffInfo
+            // 
+            this.pnlStaffInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(182)))));
+            this.pnlStaffInfo.Controls.Add(this.lblStaffId);
+            this.pnlStaffInfo.Controls.Add(this.btnEditStaffSave);
+            this.pnlStaffInfo.Controls.Add(this.lblEditStaffinfo);
+            this.pnlStaffInfo.Controls.Add(this.txtEditStaffPassword);
+            this.pnlStaffInfo.Controls.Add(this.label18);
+            this.pnlStaffInfo.Controls.Add(this.txtEditStaffEmail);
+            this.pnlStaffInfo.Controls.Add(this.label17);
+            this.pnlStaffInfo.Controls.Add(this.txtEditStaffAddress);
+            this.pnlStaffInfo.Controls.Add(this.label16);
+            this.pnlStaffInfo.Controls.Add(this.txtEditStaffMobileno);
+            this.pnlStaffInfo.Controls.Add(this.label15);
+            this.pnlStaffInfo.Controls.Add(this.txtEditStaffName);
+            this.pnlStaffInfo.Controls.Add(this.label14);
+            this.pnlStaffInfo.Controls.Add(this.txtEditStaffgender);
+            this.pnlStaffInfo.Controls.Add(this.label13);
+            this.pnlStaffInfo.Location = new System.Drawing.Point(182, -1);
+            this.pnlStaffInfo.Name = "pnlStaffInfo";
+            this.pnlStaffInfo.Size = new System.Drawing.Size(367, 461);
+            this.pnlStaffInfo.TabIndex = 232;
+            // 
+            // lblStaffId
+            // 
+            this.lblStaffId.AutoSize = true;
+            this.lblStaffId.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblStaffId.Location = new System.Drawing.Point(156, 89);
+            this.lblStaffId.Name = "lblStaffId";
+            this.lblStaffId.Size = new System.Drawing.Size(0, 37);
+            this.lblStaffId.TabIndex = 54;
+            // 
+            // btnEditStaffSave
+            // 
+            this.btnEditStaffSave.BackColor = System.Drawing.Color.Peru;
+            this.btnEditStaffSave.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditStaffSave.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnEditStaffSave.Location = new System.Drawing.Point(126, 378);
+            this.btnEditStaffSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEditStaffSave.Name = "btnEditStaffSave";
+            this.btnEditStaffSave.Size = new System.Drawing.Size(126, 38);
+            this.btnEditStaffSave.TabIndex = 53;
+            this.btnEditStaffSave.Text = "Save";
+            this.btnEditStaffSave.UseVisualStyleBackColor = false;
+            this.btnEditStaffSave.Click += new System.EventHandler(this.btnEditStaffSave_Click);
+            // 
+            // lblEditStaffinfo
+            // 
+            this.lblEditStaffinfo.AutoSize = true;
+            this.lblEditStaffinfo.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEditStaffinfo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblEditStaffinfo.Location = new System.Drawing.Point(67, 45);
+            this.lblEditStaffinfo.Name = "lblEditStaffinfo";
+            this.lblEditStaffinfo.Size = new System.Drawing.Size(261, 30);
+            this.lblEditStaffinfo.TabIndex = 34;
+            this.lblEditStaffinfo.Text = "Edit Staff Information";
+            // 
+            // txtEditStaffPassword
+            // 
+            this.txtEditStaffPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditStaffPassword.Location = new System.Drawing.Point(111, 301);
+            this.txtEditStaffPassword.Name = "txtEditStaffPassword";
+            this.txtEditStaffPassword.Size = new System.Drawing.Size(209, 20);
+            this.txtEditStaffPassword.TabIndex = 52;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label18.Location = new System.Drawing.Point(42, 148);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(63, 18);
+            this.label18.TabIndex = 40;
+            this.label18.Text = "Name :";
+            // 
+            // txtEditStaffEmail
+            // 
+            this.txtEditStaffEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditStaffEmail.Location = new System.Drawing.Point(111, 271);
+            this.txtEditStaffEmail.Name = "txtEditStaffEmail";
+            this.txtEditStaffEmail.Size = new System.Drawing.Size(209, 20);
+            this.txtEditStaffEmail.TabIndex = 51;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label17.Location = new System.Drawing.Point(28, 178);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(77, 18);
+            this.label17.TabIndex = 41;
+            this.label17.Text = "Gender :";
+            // 
+            // txtEditStaffAddress
+            // 
+            this.txtEditStaffAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditStaffAddress.Location = new System.Drawing.Point(111, 239);
+            this.txtEditStaffAddress.Name = "txtEditStaffAddress";
+            this.txtEditStaffAddress.Size = new System.Drawing.Size(209, 20);
+            this.txtEditStaffAddress.TabIndex = 50;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label16.Location = new System.Drawing.Point(37, 208);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(68, 18);
+            this.label16.TabIndex = 42;
+            this.label16.Text = "Mobile :";
+            // 
+            // txtEditStaffMobileno
+            // 
+            this.txtEditStaffMobileno.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditStaffMobileno.Location = new System.Drawing.Point(111, 207);
+            this.txtEditStaffMobileno.Name = "txtEditStaffMobileno";
+            this.txtEditStaffMobileno.Size = new System.Drawing.Size(209, 20);
+            this.txtEditStaffMobileno.TabIndex = 49;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label15.Location = new System.Drawing.Point(21, 239);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(84, 18);
+            this.label15.TabIndex = 43;
+            this.label15.Text = "Address :";
+            // 
+            // txtEditStaffName
+            // 
+            this.txtEditStaffName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditStaffName.Location = new System.Drawing.Point(111, 146);
+            this.txtEditStaffName.Name = "txtEditStaffName";
+            this.txtEditStaffName.Size = new System.Drawing.Size(209, 20);
+            this.txtEditStaffName.TabIndex = 48;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label14.Location = new System.Drawing.Point(9, 301);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(96, 18);
+            this.label14.TabIndex = 44;
+            this.label14.Text = "Password :";
+            // 
+            // txtEditStaffgender
+            // 
+            this.txtEditStaffgender.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditStaffgender.Location = new System.Drawing.Point(111, 179);
+            this.txtEditStaffgender.Name = "txtEditStaffgender";
+            this.txtEditStaffgender.Size = new System.Drawing.Size(209, 20);
+            this.txtEditStaffgender.TabIndex = 47;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label13.Location = new System.Drawing.Point(45, 270);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(60, 18);
+            this.label13.TabIndex = 45;
+            this.label13.Text = "Email :";
+            // 
+            // EditStaffProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.pnlStaffEditInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "EditStaffProfile";
+            this.Text = "EditStaffProfile";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.EditStaffProfile_FormClosed);
+            this.pnlStaffEditInfo.ResumeLayout(false);
+            this.pnlStaffInfo.ResumeLayout(false);
+            this.pnlStaffInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlStaffEditInfo;
+        private System.Windows.Forms.Button btnEditStaffSave;
+        private System.Windows.Forms.TextBox txtEditStaffPassword;
+        private System.Windows.Forms.TextBox txtEditStaffEmail;
+        private System.Windows.Forms.TextBox txtEditStaffAddress;
+        private System.Windows.Forms.TextBox txtEditStaffMobileno;
+        private System.Windows.Forms.TextBox txtEditStaffName;
+        private System.Windows.Forms.TextBox txtEditStaffgender;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label lblEditStaffinfo;
+        private System.Windows.Forms.Panel pnlStaffInfo;
+        private System.Windows.Forms.Label lblStaffId;
+    }
+}
---
+++ OOP2FinalProject/EditStaffProfile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP2FinalProject
+{
+    public partial class EditStaffProfile : Form
+    {
+        private DataAccess Da { get; set; }
+        public EditStaffProfile()
+        {
+            InitializeComponent();
+        }
+        public EditStaffProfile(string id)
+        {
+            InitializeComponent();
+            this.Da = new DataAccess();
+            var ds = this.Da.ExecuteQuery("select * from StaffInfo where S_Id = '" + id + "'");
+            this.lblStaffId.Text = id;
+            this.txtEditStaffName.Text = ds.Tables[0].Rows[0][1].ToString();
+            this.txtEditStaffgender.Text = ds.Tables[0].Rows[0][2].ToString();
+            this.txtEditStaffMobileno.Text = ds.Tables[0].Rows[0][3].ToString();
+            this.txtEditStaffAddress.Text = ds.Tables[0].Rows[0][4].ToString();
+            this.txtEditStaffEmail.Text = ds.Tables[0].Rows[0][7].ToString();
+            this.txtEditStaffPassword.Text = ds.Tables[0].Rows[0][8].ToString();
+        }
+
+        private void btnEditStaffSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sql = @"update StaffInfo
+                           set S_Name = '" + this.txtEditStaffName.Text + @"',
+                           S_Gender = '" + this.txtEditStaffgender.Text + @"',
+                           S_Mobile = '" + this.txtEditStaffMobileno.Text + @"',
+                           S_Address = '" + this.txtEditStaffAddress.Text + @"',
+                           S_Email = '" + this.txtEditStaffEmail.Text + @"',
+                           S_Password = '" + this.txtEditStaffPassword.Text + @"'
+                           where S_Id = '" + this.lblStaffId.Text + "'; ";
+                 var count = this.Da.ExecuteDMLQuery(sql);
+                if (count == 1) 
+                { 
+                    MessageBox.Show("Data has been updated");
+                    Staff staff = new Staff(this.lblStaffId.Text);
+                    staff.Visible = true;
+                    staff.Location = new Point(this.Location.X, this.Location.Y);
+                    this.Hide();
+                }
+                else { MessageBox.Show("Data hasn't been updated"); }
+            }
+            catch(Exception exc) { MessageBox.Show("An error has occured: " + exc.Message); }
+        }
+
+        private void EditStaffProfile_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
