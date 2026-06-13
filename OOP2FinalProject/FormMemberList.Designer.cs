cefce23cc3c16f388c48273976d1cf76e151def8
rakinasraf <rakinasraf128@gmail.com>
2026-06-13 13:03:37 +0600

    Implementation of Member List Form

---
+++ OOP2FinalProject/FormMemberList.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace OOP2FinalProject
+{
+    partial class FormMemberList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.pnlAdmin = new System.Windows.Forms.Panel();
+            this.txtAutoSearch = new System.Windows.Forms.TextBox();
+            this.dgvMemberList = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblMobile = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblUserId = new System.Windows.Forms.Label();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.pnlAdmin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMemberList)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlAdmin
+            // 
+            this.pnlAdmin.BackColor = System.Drawing.Color.DarkKhaki;
+            this.pnlAdmin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlAdmin.Controls.Add(this.txtAutoSearch);
+            this.pnlAdmin.Controls.Add(this.dgvMemberList);
+            this.pnlAdmin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlAdmin.Location = new System.Drawing.Point(0, 0);
+            this.pnlAdmin.Name = "pnlAdmin";
+            this.pnlAdmin.Size = new System.Drawing.Size(734, 272);
+            this.pnlAdmin.TabIndex = 5;
+            this.pnlAdmin.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlAdmin_Paint);
+            // 
+            // txtAutoSearch
+            // 
+            this.txtAutoSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAutoSearch.Location = new System.Drawing.Point(555, 11);
+            this.txtAutoSearch.Name = "txtAutoSearch";
+            this.txtAutoSearch.Size = new System.Drawing.Size(166, 26);
+            this.txtAutoSearch.TabIndex = 13;
+            // 
+            // dgvMemberList
+            // 
+            this.dgvMemberList.AllowUserToAddRows = false;
+            this.dgvMemberList.AllowUserToDeleteRows = false;
+            this.dgvMemberList.AllowUserToResizeColumns = false;
+            this.dgvMemberList.AllowUserToResizeRows = false;
+            this.dgvMemberList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvMemberList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dgvMemberList.BackgroundColor = System.Drawing.Color.White;
+            this.dgvMemberList.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvMemberList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvMemberList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMemberList.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvMemberList.Location = new System.Drawing.Point(0, 46);
+            this.dgvMemberList.Margin = new System.Windows.Forms.Padding(0);
+            this.dgvMemberList.Name = "dgvMemberList";
+            this.dgvMemberList.ReadOnly = true;
+            this.dgvMemberList.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvMemberList.RowHeadersVisible = false;
+            this.dgvMemberList.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dgvMemberList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvMemberList.Size = new System.Drawing.Size(732, 224);
+            this.dgvMemberList.TabIndex = 12;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.btnInsert);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.lblPassword);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.lblMobile);
+            this.panel1.Controls.Add(this.txtMobile);
+            this.panel1.Controls.Add(this.lblPosition);
+            this.panel1.Controls.Add(this.txtPosition);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.lblUserId);
+            this.panel1.Controls.Add(this.txtUserId);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 274);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(734, 187);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Location = new System.Drawing.Point(593, 83);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 28);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(498, 35);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 12;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(568, 32);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // lblMobile
+            // 
+            this.lblMobile.AutoSize = true;
+            this.lblMobile.Location = new System.Drawing.Point(282, 86);
+            this.lblMobile.Name = "lblMobile";
+            this.lblMobile.Size = new System.Drawing.Size(38, 13);
+            this.lblMobile.TabIndex = 10;
+            this.lblMobile.Text = "Mobile";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(352, 83);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(100, 20);
+            this.txtMobile.TabIndex = 9;
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(269, 32);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(44, 13);
+            this.lblPosition.TabIndex = 8;
+            this.lblPosition.Text = "Position";
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(339, 29);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(100, 20);
+            this.txtPosition.TabIndex = 7;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(45, 78);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 6;
+            this.lblName.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(115, 75);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // lblUserId
+            // 
+            this.lblUserId.AutoSize = true;
+            this.lblUserId.Location = new System.Drawing.Point(45, 32);
+            this.lblUserId.Name = "lblUserId";
+            this.lblUserId.Size = new System.Drawing.Size(41, 13);
+            this.lblUserId.TabIndex = 4;
+            this.lblUserId.Text = "User Id";
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(115, 29);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(100, 20);
+            this.txtUserId.TabIndex = 3;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.Black;
+            this.btnBack.Location = new System.Drawing.Point(22, 147);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 28);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnInsert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.ForeColor = System.Drawing.Color.Black;
+            this.btnInsert.Location = new System.Drawing.Point(492, 83);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 28);
+            this.btnInsert.TabIndex = 15;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // FormMemberList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlAdmin);
+            this.Name = "FormMemberList";
+            this.Text = "MemberList";
+            this.pnlAdmin.ResumeLayout(false);
+            this.pnlAdmin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMemberList)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlAdmin;
+        private System.Windows.Forms.TextBox txtAutoSearch;
+        private System.Windows.Forms.DataGridView dgvMemberList;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblMobile;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblUserId;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnInsert;
+    }
+}
---
+++ OOP2FinalProject/FormMemberList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP2FinalProject
+{
+    public partial class FormMemberList : Form
+    {
+        public FormMemberList()
+        {
+            InitializeComponent();
+            SqlConnection sqlcon = new SqlConnection("Data Source=LAPTOP-6I8C99IJ\\SQLEXPRESS;Initial Catalog=UserInfo;Integrated Security=True");
+            sqlcon.Open();
+            //string sql = "select * from UserInfo";
+            //SqlCommand sqlcom = new SqlCommand("select * from UserInfo", sqlcon);
+            string sql = "select * from StaffInfo";
+
+            //SqlCommand sqlcom = new SqlCommand(sql, sqlcon);
+            SqlDataAdapter sda = new SqlDataAdapter(sql, sqlcon);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            dgvMemberList.DataSource = ds.Tables[0];
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void pnlAdmin_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!this.IsValidToSave())
+                {
+                    MessageBox.Show("Please fill all the empty fields");
+                    return;
+                }
+
+                var query = "select * from Movie where Id ='" + this.txtID.Text + "';";
+                var dt = this.Da.ExecuteQueryTable(query);
+
+                if (dt.Rows.Count == 1)
+                {
+                    //update
+                    var sql = @"update Movie
+                                set Title = '" + this.txtTitle.Text + @"',
+                                IMDB = " + this.txtIMDB.Text + @",
+                                Income = " + this.txtIncome.Text + @",
+                                ReleaseDate = '" + this.dtpReleaseDate.Text + @"',
+                                Genre = '" + this.cmbGenre.Text + @"'
+                                where Id = '" + this.txtID.Text + "'; ";
+                    var count = this.Da.ExecuteDMLQuery(sql);
+
+                    if (count == 1)
+                        MessageBox.Show("Data has been updated");
+                    else
+                        MessageBox.Show("Data hasn't been updated");
+                }
+                else
+                {
+                    //insert
+                    var sql = "insert into Movie values('" + this.txtID.Text + "', '" + this.txtTitle.Text + "', " + this.txtIMDB.Text + ", " + this.txtIncome.Text + ", '" + this.dtpReleaseDate.Text + "', '" + this.cmbGenre.Text + "');";
+                    var count = this.Da.ExecuteDMLQuery(sql);
+
+                    if (count == 1)
+                        MessageBox.Show("Data has been added");
+                    else
+                        MessageBox.Show("Data hasn't been added");
+
+                }
+
+                this.PopulateGridView();
+                this.ClearAll();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error has occured: " + exc.Message);
+            }
+        }
+        private bool IsValidToSave()
+        {
+            if (string.IsNullOrEmpty(this.txtUserId.Text) || string.IsNullOrEmpty(this.txtName.Text) ||
+                string.IsNullOrEmpty(this.txtPrice.Text) || string.IsNullOrEmpty(this.txt.Text) ||
+                string.IsNullOrEmpty(this.cmbGenre.Text))
+                return false;
+            else
+                return true;
+        }
+    }
+}
