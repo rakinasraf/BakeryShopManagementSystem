c019da2cdfe93c79739cc392ece8f32e51957aa0
rakinasraf <rakinasraf128@gmail.com>
2026-06-13 12:52:27 +0600

    Update Admin profile form

---
+++ OOP2FinalProject/EditAdminProfile.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace OOP2FinalProject
+{
+    partial class EditAdminProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlStaffEditInfo = new System.Windows.Forms.Panel();
+            this.pnlStaffInfo = new System.Windows.Forms.Panel();
+            this.lblAdminId = new System.Windows.Forms.Label();
+            this.btnEditAdminSave = new System.Windows.Forms.Button();
+            this.lblEditAdmininfo = new System.Windows.Forms.Label();
+            this.txtEditAdminPassword = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.txtEditAdminEmail = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.txtEditAdminAddress = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.txtEditAdminMobileno = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.txtEditAdminName = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtEditAdmingender = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.pnlStaffEditInfo.SuspendLayout();
+            this.pnlStaffInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlStaffEditInfo
+            // 
+            this.pnlStaffEditInfo.BackColor = System.Drawing.Color.Peru;
+            this.pnlStaffEditInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlStaffEditInfo.Controls.Add(this.pnlStaffInfo);
+            this.pnlStaffEditInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlStaffEditInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlStaffEditInfo.Location = new System.Drawing.Point(0, 0);
+            this.pnlStaffEditInfo.Name = "pnlStaffEditInfo";
+            this.pnlStaffEditInfo.Size = new System.Drawing.Size(734, 461);
+            this.pnlStaffEditInfo.TabIndex = 232;
+            // 
+            // pnlStaffInfo
+            // 
+            this.pnlStaffInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(182)))));
+            this.pnlStaffInfo.Controls.Add(this.lblAdminId);
+            this.pnlStaffInfo.Controls.Add(this.btnEditAdminSave);
+            this.pnlStaffInfo.Controls.Add(this.lblEditAdmininfo);
+            this.pnlStaffInfo.Controls.Add(this.txtEditAdminPassword);
+            this.pnlStaffInfo.Controls.Add(this.label18);
+            this.pnlStaffInfo.Controls.Add(this.txtEditAdminEmail);
+            this.pnlStaffInfo.Controls.Add(this.label17);
+            this.pnlStaffInfo.Controls.Add(this.txtEditAdminAddress);
+            this.pnlStaffInfo.Controls.Add(this.label16);
+            this.pnlStaffInfo.Controls.Add(this.txtEditAdminMobileno);
+            this.pnlStaffInfo.Controls.Add(this.label15);
+            this.pnlStaffInfo.Controls.Add(this.txtEditAdminName);
+            this.pnlStaffInfo.Controls.Add(this.label14);
+            this.pnlStaffInfo.Controls.Add(this.txtEditAdmingender);
+            this.pnlStaffInfo.Controls.Add(this.label13);
+            this.pnlStaffInfo.Location = new System.Drawing.Point(182, -1);
+            this.pnlStaffInfo.Name = "pnlStaffInfo";
+            this.pnlStaffInfo.Size = new System.Drawing.Size(367, 461);
+            this.pnlStaffInfo.TabIndex = 232;
+            // 
+            // lblAdminId
+            // 
+            this.lblAdminId.AutoSize = true;
+            this.lblAdminId.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblAdminId.Location = new System.Drawing.Point(156, 89);
+            this.lblAdminId.Name = "lblAdminId";
+            this.lblAdminId.Size = new System.Drawing.Size(0, 37);
+            this.lblAdminId.TabIndex = 54;
+            this.lblAdminId.Visible = false;
+            // 
+            // btnEditAdminSave
+            // 
+            this.btnEditAdminSave.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnEditAdminSave.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditAdminSave.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnEditAdminSave.Location = new System.Drawing.Point(123, 383);
+            this.btnEditAdminSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEditAdminSave.Name = "btnEditAdminSave";
+            this.btnEditAdminSave.Size = new System.Drawing.Size(126, 38);
+            this.btnEditAdminSave.TabIndex = 53;
+            this.btnEditAdminSave.Text = "Save";
+            this.btnEditAdminSave.UseVisualStyleBackColor = false;
+            this.btnEditAdminSave.Click += new System.EventHandler(this.btnEditAdminSave_Click);
+            // 
+            // lblEditAdmininfo
+            // 
+            this.lblEditAdmininfo.AutoSize = true;
+            this.lblEditAdmininfo.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEditAdmininfo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblEditAdmininfo.Location = new System.Drawing.Point(49, 45);
+            this.lblEditAdmininfo.Name = "lblEditAdmininfo";
+            this.lblEditAdmininfo.Size = new System.Drawing.Size(277, 30);
+            this.lblEditAdmininfo.TabIndex = 34;
+            this.lblEditAdmininfo.Text = "Edit Admin Information";
+            // 
+            // txtEditAdminPassword
+            // 
+            this.txtEditAdminPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditAdminPassword.Location = new System.Drawing.Point(111, 301);
+            this.txtEditAdminPassword.Name = "txtEditAdminPassword";
+            this.txtEditAdminPassword.Size = new System.Drawing.Size(209, 20);
+            this.txtEditAdminPassword.TabIndex = 52;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label18.Location = new System.Drawing.Point(42, 148);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(58, 18);
+            this.label18.TabIndex = 40;
+            this.label18.Text = "Name :";
+            // 
+            // txtEditAdminEmail
+            // 
+            this.txtEditAdminEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditAdminEmail.Location = new System.Drawing.Point(111, 271);
+            this.txtEditAdminEmail.Name = "txtEditAdminEmail";
+            this.txtEditAdminEmail.Size = new System.Drawing.Size(209, 20);
+            this.txtEditAdminEmail.TabIndex = 51;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label17.Location = new System.Drawing.Point(28, 178);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(69, 18);
+            this.label17.TabIndex = 41;
+            this.label17.Text = "Gender :";
+            // 
+            // txtEditAdminAddress
+            // 
+            this.txtEditAdminAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditAdminAddress.Location = new System.Drawing.Point(111, 239);
+            this.txtEditAdminAddress.Name = "txtEditAdminAddress";
+            this.txtEditAdminAddress.Size = new System.Drawing.Size(209, 20);
+            this.txtEditAdminAddress.TabIndex = 50;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label16.Location = new System.Drawing.Point(37, 208);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(63, 18);
+            this.label16.TabIndex = 42;
+            this.label16.Text = "Mobile :";
+            // 
+            // txtEditAdminMobileno
+            // 
+            this.txtEditAdminMobileno.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditAdminMobileno.Location = new System.Drawing.Point(111, 207);
+            this.txtEditAdminMobileno.Name = "txtEditAdminMobileno";
+            this.txtEditAdminMobileno.Size = new System.Drawing.Size(209, 20);
+            this.txtEditAdminMobileno.TabIndex = 49;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label15.Location = new System.Drawing.Point(21, 239);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(75, 18);
+            this.label15.TabIndex = 43;
+            this.label15.Text = "Address :";
+            // 
+            // txtEditAdminName
+            // 
+            this.txtEditAdminName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditAdminName.Location = new System.Drawing.Point(111, 146);
+            this.txtEditAdminName.Name = "txtEditAdminName";
+            this.txtEditAdminName.Size = new System.Drawing.Size(209, 20);
+            this.txtEditAdminName.TabIndex = 48;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label14.Location = new System.Drawing.Point(9, 301);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(86, 18);
+            this.label14.TabIndex = 44;
+            this.label14.Text = "Password :";
+            // 
+            // txtEditAdmingender
+            // 
+            this.txtEditAdmingender.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditAdmingender.Location = new System.Drawing.Point(111, 179);
+            this.txtEditAdmingender.Name = "txtEditAdmingender";
+            this.txtEditAdmingender.Size = new System.Drawing.Size(209, 20);
+            this.txtEditAdmingender.TabIndex = 47;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label13.Location = new System.Drawing.Point(45, 270);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(55, 18);
+            this.label13.TabIndex = 45;
+            this.label13.Text = "Email :";
+            // 
+            // EditAdminProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.pnlStaffEditInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "EditAdminProfile";
+            this.Text = "EditAminProfile";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.EditAdminProfile_FormClosed);
+            this.pnlStaffEditInfo.ResumeLayout(false);
+            this.pnlStaffInfo.ResumeLayout(false);
+            this.pnlStaffInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlStaffEditInfo;
+        private System.Windows.Forms.Panel pnlStaffInfo;
+        private System.Windows.Forms.Label lblAdminId;
+        private System.Windows.Forms.Button btnEditAdminSave;
+        private System.Windows.Forms.Label lblEditAdmininfo;
+        private System.Windows.Forms.TextBox txtEditAdminPassword;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.TextBox txtEditAdminEmail;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox txtEditAdminAddress;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox txtEditAdminMobileno;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox txtEditAdminName;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtEditAdmingender;
+        private System.Windows.Forms.Label label13;
+    }
+}
---
+++ OOP2FinalProject/EditAdminProfile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP2FinalProject
+{
+    public partial class EditAdminProfile : Form
+    {
+        private DataAccess Da { get; set; }
+        public EditAdminProfile()
+        {
+            InitializeComponent();
+        }
+        public EditAdminProfile(string id)
+        {
+            InitializeComponent();
+            this.Da = new DataAccess();
+            var ds = this.Da.ExecuteQuery("select * from AdminInfo where A_Id = '" + id + "'");
+            this.lblAdminId.Text = id;
+            this.txtEditAdminName.Text = ds.Tables[0].Rows[0][1].ToString();
+            this.txtEditAdmingender.Text = ds.Tables[0].Rows[0][2].ToString();
+            this.txtEditAdminMobileno.Text = ds.Tables[0].Rows[0][3].ToString();
+            this.txtEditAdminAddress.Text = ds.Tables[0].Rows[0][4].ToString();
+            this.txtEditAdminEmail.Text = ds.Tables[0].Rows[0][5].ToString();
+            this.txtEditAdminPassword.Text = ds.Tables[0].Rows[0][6].ToString();
+        }
+
+        private void btnEditAdminSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sql = @"update AdminInfo
+                           set A_Name = '" + this.txtEditAdminName.Text + @"',
+                           A_Gender = '" + this.txtEditAdmingender.Text + @"',
+                           A_Mobile = '" + this.txtEditAdminMobileno.Text + @"',
+                           A_Address = '" + this.txtEditAdminAddress.Text + @"',
+                           A_Email = '" + this.txtEditAdminEmail.Text + @"',
+                           A_Password = '" + this.txtEditAdminPassword.Text + @"'
+                           where A_Id = '" + this.lblAdminId.Text + "'; ";
+                var count = this.Da.ExecuteDMLQuery(sql);
+                if (count == 1)
+                {
+                    MessageBox.Show("Data has been updated");
+                    Admin admin = new Admin(this.lblAdminId.Text);
+                    admin.Visible = true;
+                    admin.Location = new Point(this.Location.X, this.Location.Y);
+                    this.Hide();
+                }
+                else { MessageBox.Show("Data hasn't been updated"); }
+                
+            }
+            catch (Exception exc) { MessageBox.Show("An error has occured: " + exc.Message); }
+        }
+        private void EditAdminProfile_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
