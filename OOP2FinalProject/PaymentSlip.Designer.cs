563d0d9b4b388589ab895e8603b06c88ca023ede
rakinasraf <rakinasraf128@gmail.com>
2026-06-13 13:50:26 +0600

    Implementation of Payment Slip Form

---
+++ OOP2FinalProject/PaymentSlip.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace OOP2FinalProject
+{
+    partial class PaymentSlip
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlPayslip = new System.Windows.Forms.Panel();
+            this.lblPaymentDate = new System.Windows.Forms.Label();
+            this.lblStaffId = new System.Windows.Forms.Label();
+            this.lblthankyou = new System.Windows.Forms.Label();
+            this.lblStaffName = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTotalAmount = new System.Windows.Forms.Label();
+            this.lblCustomerMobile = new System.Windows.Forms.Label();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblPaymentId = new System.Windows.Forms.Label();
+            this.btnGoToHome = new System.Windows.Forms.Button();
+            this.lblpayslip = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlPayslip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlPayslip
+            // 
+            this.pnlPayslip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(182)))));
+            this.pnlPayslip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlPayslip.Controls.Add(this.lblPaymentDate);
+            this.pnlPayslip.Controls.Add(this.lblStaffId);
+            this.pnlPayslip.Controls.Add(this.lblthankyou);
+            this.pnlPayslip.Controls.Add(this.lblStaffName);
+            this.pnlPayslip.Controls.Add(this.label3);
+            this.pnlPayslip.Controls.Add(this.lblTotalAmount);
+            this.pnlPayslip.Controls.Add(this.lblCustomerMobile);
+            this.pnlPayslip.Controls.Add(this.lblCustomerName);
+            this.pnlPayslip.Controls.Add(this.lblPaymentId);
+            this.pnlPayslip.Controls.Add(this.btnGoToHome);
+            this.pnlPayslip.Controls.Add(this.lblpayslip);
+            this.pnlPayslip.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlPayslip.Location = new System.Drawing.Point(189, 0);
+            this.pnlPayslip.Name = "pnlPayslip";
+            this.pnlPayslip.Size = new System.Drawing.Size(367, 461);
+            this.pnlPayslip.TabIndex = 234;
+            // 
+            // lblPaymentDate
+            // 
+            this.lblPaymentDate.AutoSize = true;
+            this.lblPaymentDate.BackColor = System.Drawing.Color.Transparent;
+            this.lblPaymentDate.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaymentDate.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblPaymentDate.Location = new System.Drawing.Point(13, 185);
+            this.lblPaymentDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPaymentDate.Name = "lblPaymentDate";
+            this.lblPaymentDate.Size = new System.Drawing.Size(53, 23);
+            this.lblPaymentDate.TabIndex = 82;
+            this.lblPaymentDate.Text = "Date: ";
+            // 
+            // lblStaffId
+            // 
+            this.lblStaffId.AutoSize = true;
+            this.lblStaffId.BackColor = System.Drawing.Color.Transparent;
+            this.lblStaffId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffId.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblStaffId.Location = new System.Drawing.Point(105, 283);
+            this.lblStaffId.Name = "lblStaffId";
+            this.lblStaffId.Size = new System.Drawing.Size(0, 20);
+            this.lblStaffId.TabIndex = 81;
+            this.lblStaffId.Visible = false;
+            // 
+            // lblthankyou
+            // 
+            this.lblthankyou.AutoSize = true;
+            this.lblthankyou.BackColor = System.Drawing.Color.Transparent;
+            this.lblthankyou.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblthankyou.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblthankyou.Location = new System.Drawing.Point(84, 321);
+            this.lblthankyou.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblthankyou.Name = "lblthankyou";
+            this.lblthankyou.Size = new System.Drawing.Size(207, 23);
+            this.lblthankyou.TabIndex = 80;
+            this.lblthankyou.Text = "Thank\'s for the Shopping!";
+            // 
+            // lblStaffName
+            // 
+            this.lblStaffName.AutoSize = true;
+            this.lblStaffName.BackColor = System.Drawing.Color.Transparent;
+            this.lblStaffName.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblStaffName.Location = new System.Drawing.Point(105, 260);
+            this.lblStaffName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStaffName.Name = "lblStaffName";
+            this.lblStaffName.Size = new System.Drawing.Size(0, 23);
+            this.lblStaffName.TabIndex = 79;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(15, 260);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 23);
+            this.label3.TabIndex = 78;
+            this.label3.Text = "Issued By:";
+            // 
+            // lblTotalAmount
+            // 
+            this.lblTotalAmount.AutoSize = true;
+            this.lblTotalAmount.BackColor = System.Drawing.Color.Transparent;
+            this.lblTotalAmount.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalAmount.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTotalAmount.Location = new System.Drawing.Point(13, 213);
+            this.lblTotalAmount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotalAmount.Name = "lblTotalAmount";
+            this.lblTotalAmount.Size = new System.Drawing.Size(121, 23);
+            this.lblTotalAmount.TabIndex = 76;
+            this.lblTotalAmount.Text = "Total Amount: ";
+            // 
+            // lblCustomerMobile
+            // 
+            this.lblCustomerMobile.AutoSize = true;
+            this.lblCustomerMobile.BackColor = System.Drawing.Color.Transparent;
+            this.lblCustomerMobile.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomerMobile.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblCustomerMobile.Location = new System.Drawing.Point(13, 156);
+            this.lblCustomerMobile.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCustomerMobile.Name = "lblCustomerMobile";
+            this.lblCustomerMobile.Size = new System.Drawing.Size(171, 23);
+            this.lblCustomerMobile.TabIndex = 74;
+            this.lblCustomerMobile.Text = "Customer Phone No: ";
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.BackColor = System.Drawing.Color.Transparent;
+            this.lblCustomerName.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomerName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblCustomerName.Location = new System.Drawing.Point(13, 127);
+            this.lblCustomerName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(140, 23);
+            this.lblCustomerName.TabIndex = 71;
+            this.lblCustomerName.Text = "Customer Name: ";
+            // 
+            // lblPaymentId
+            // 
+            this.lblPaymentId.AutoSize = true;
+            this.lblPaymentId.BackColor = System.Drawing.Color.Transparent;
+            this.lblPaymentId.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaymentId.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblPaymentId.Location = new System.Drawing.Point(13, 98);
+            this.lblPaymentId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPaymentId.Name = "lblPaymentId";
+            this.lblPaymentId.Size = new System.Drawing.Size(74, 23);
+            this.lblPaymentId.TabIndex = 70;
+            this.lblPaymentId.Text = "Invoice: ";
+            // 
+            // btnGoToHome
+            // 
+            this.btnGoToHome.BackColor = System.Drawing.Color.Peru;
+            this.btnGoToHome.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoToHome.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnGoToHome.Location = new System.Drawing.Point(120, 365);
+            this.btnGoToHome.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGoToHome.Name = "btnGoToHome";
+            this.btnGoToHome.Size = new System.Drawing.Size(126, 38);
+            this.btnGoToHome.TabIndex = 60;
+            this.btnGoToHome.Text = "Go to Home";
+            this.btnGoToHome.UseVisualStyleBackColor = false;
+            this.btnGoToHome.Click += new System.EventHandler(this.btnGoToHome_Click);
+            // 
+            // lblpayslip
+            // 
+            this.lblpayslip.AutoSize = true;
+            this.lblpayslip.BackColor = System.Drawing.Color.Transparent;
+            this.lblpayslip.Font = new System.Drawing.Font("Arial Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpayslip.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblpayslip.Location = new System.Drawing.Point(84, 23);
+            this.lblpayslip.Name = "lblpayslip";
+            this.lblpayslip.Size = new System.Drawing.Size(212, 38);
+            this.lblpayslip.TabIndex = 44;
+            this.lblpayslip.Text = "Payment Slip";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Peru;
+            this.panel1.Controls.Add(this.pnlPayslip);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(734, 461);
+            this.panel1.TabIndex = 235;
+            // 
+            // PaymentSlip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "PaymentSlip";
+            this.Text = "PaymentSlip";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.PaymentSlip_FormClosed);
+            this.pnlPayslip.ResumeLayout(false);
+            this.pnlPayslip.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPayslip;
+        private System.Windows.Forms.Label lblthankyou;
+        private System.Windows.Forms.Label lblStaffName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTotalAmount;
+        private System.Windows.Forms.Label lblCustomerMobile;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblPaymentId;
+        private System.Windows.Forms.Button btnGoToHome;
+        private System.Windows.Forms.Label lblpayslip;
+        private System.Windows.Forms.Label lblStaffId;
+        private System.Windows.Forms.Label lblPaymentDate;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ OOP2FinalProject/PaymentSlip.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP2FinalProject
+{
+    public partial class PaymentSlip : Form
+    {
+        private DataAccess Da { get; set; }
+        public PaymentSlip()
+        {
+            InitializeComponent();
+        }
+        public PaymentSlip(string sid, string pid)
+        {
+            InitializeComponent();
+            this.lblStaffId.Text = sid;
+            this.Da = new DataAccess();
+
+            var ds1 = this.Da.ExecuteQuery("select * from PaymentList where Payment_Id = '" + pid + "'");
+            this.lblPaymentId.Text += ds1.Tables[0].Rows[0][0].ToString();
+            this.lblCustomerName.Text += ds1.Tables[0].Rows[0][1].ToString();
+            this.lblCustomerMobile.Text += ds1.Tables[0].Rows[0][2].ToString();
+            this.lblTotalAmount.Text += ds1.Tables[0].Rows[0][3].ToString();
+            this.lblPaymentDate.Text += ds1.Tables[0].Rows[0][4].ToString();
+
+            var ds2 = this.Da.ExecuteQuery("select * from StaffInfo where S_Id = '" + sid + "'");
+            this.lblStaffName.Text = ds2.Tables[0].Rows[0][1].ToString();
+        }
+
+        private void btnGoToHome_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                this.Da.ExecuteDDLQuery("TRUNCATE TABLE OrderDetails");
+                MessageBox.Show("Done");
+                Staff staff = new Staff(this.lblStaffId.Text);
+                staff.Visible = true;
+                staff.Location = new Point(this.Location.X, this.Location.Y);
+                this.Hide();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error has occured: " + exc.Message);
+            }
+        }
+
+        private void PaymentSlip_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
