754b5e510ffe1c7e9bb93aafe15fde3aec06e395
rakinasraf <rakinasraf128@gmail.com>
2026-06-13 13:00:14 +0600

    Implementation of Login Form

---
+++ OOP2FinalProject/FormLogIn.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace OOP2FinalProject
+{
+    partial class FormLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLogIn = new System.Windows.Forms.Panel();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUserId = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.pnlLogIn.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLogIn
+            // 
+            this.pnlLogIn.BackColor = System.Drawing.Color.DarkKhaki;
+            this.pnlLogIn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLogIn.Controls.Add(this.lblPassword);
+            this.pnlLogIn.Controls.Add(this.lblUserId);
+            this.pnlLogIn.Controls.Add(this.label1);
+            this.pnlLogIn.Controls.Add(this.btnLogIn);
+            this.pnlLogIn.Controls.Add(this.txtPassword);
+            this.pnlLogIn.Controls.Add(this.txtUserId);
+            this.pnlLogIn.Location = new System.Drawing.Point(228, 89);
+            this.pnlLogIn.Name = "pnlLogIn";
+            this.pnlLogIn.Size = new System.Drawing.Size(300, 300);
+            this.pnlLogIn.TabIndex = 0;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(125, 153);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(71, 15);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblUserId
+            // 
+            this.lblUserId.AutoSize = true;
+            this.lblUserId.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserId.Location = new System.Drawing.Point(134, 87);
+            this.lblUserId.Name = "lblUserId";
+            this.lblUserId.Size = new System.Drawing.Size(53, 15);
+            this.lblUserId.TabIndex = 4;
+            this.lblUserId.Text = "User Id";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(74, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 44);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LOG  IN";
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogIn.ForeColor = System.Drawing.Color.Black;
+            this.btnLogIn.Location = new System.Drawing.Point(121, 234);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 28);
+            this.btnLogIn.TabIndex = 2;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = false;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.White;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(56, 177);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(0);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(200, 29);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.BackColor = System.Drawing.Color.White;
+            this.txtUserId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUserId.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserId.Location = new System.Drawing.Point(56, 110);
+            this.txtUserId.Margin = new System.Windows.Forms.Padding(0);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(200, 29);
+            this.txtUserId.TabIndex = 0;
+            this.txtUserId.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // FormLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.pnlLogIn);
+            this.Name = "FormLogIn";
+            this.Text = "LogIn";
+            this.Load += new System.EventHandler(this.FormLogIn_Load);
+            this.pnlLogIn.ResumeLayout(false);
+            this.pnlLogIn.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogIn;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUserId;
+    }
+}
+
---
+++ OOP2FinalProject/FormLogIn.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Server;
+
+namespace OOP2FinalProject
+{
+    public partial class FormLogIn : Form
+    {
+        public FormLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            //this.txtPassword.Text = string.Empty;
+            //this.txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void txtUserId_Click(object sender, EventArgs e)
+        {
+            //this.txtUserId.Text = string.Empty;
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection("Data Source=LAPTOP-6I8C99IJ\\SQLEXPRESS;Initial Catalog=UserInfo;Integrated Security=True");
+            sqlcon.Open();
+            //string sql = "select * from UserInfo";
+            //SqlCommand sqlcom = new SqlCommand("select * from UserInfo", sqlcon);
+            string sql = "select * from UserInfo where Id = '" + this.txtUserId.Text + "' and Password = '" + this.txtPassword.Text + "'";
+
+            //SqlCommand sqlcom = new SqlCommand(sql, sqlcon);
+            SqlDataAdapter sda = new SqlDataAdapter(sql,sqlcon);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count == 1)
+            {
+                    if (ds.Tables[0].Rows[0][2].ToString() == "Admin")
+                    {
+                        FormAdmin fa = new FormAdmin(this.txtUserId.Text);
+                        fa.Visible = true;
+                    }
+                    else
+                    {
+                        FormMember fm = new FormMember(this.txtUserId.Text);
+                        fm.Show();
+                    }
+            }
+            else
+            {
+                    MessageBox.Show("Invalid User");
+            }
+            sqlcon.Close();
+        }
+
+        private void FormLogIn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
